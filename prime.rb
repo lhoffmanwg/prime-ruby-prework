d104c921c9b4461f0f14ee6bab61f1fd45b1ced4
Laura Hoffman <lhoffmanwg@gmail.com>
2019-02-07 06:02:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 def prime?(integer)
+  if integer < 0 
+    false
+  end
+  
  (2..integer - 1).each do |x|
   if (integer % x) == 0
    return false
-  else
-   return true
   end
  end
  return true
